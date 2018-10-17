351aab905599366336504dab1102796daa9490bd
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-17 09:51:49 -0600

    Creating a station_search model.

---
+++ app/models/station_search.rb
@@ -0,0 +1,19 @@
+class StationSearch
+  attr_reader :zipcode
+
+  def initialize(zipcode)
+    @zipcode = zipcode
+  end
+
+  def stations
+    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{zipcode}&fuel_type=ELEC,LPG&limit=10")
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["X-Api-Key"] = "#{ENV["nrel_api_key"]}"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+end
--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -19,6 +19,8 @@ feature "User can visit root page" do
     # Then I should be on page "/search"
     expect(current_path).to eq('/search')
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    station_search = StationSearch.new
+    expect(station_search.stations.count).to eq(10)
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
     within(first(".station")) do
