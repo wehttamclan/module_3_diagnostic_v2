12944188be4a01e32ea21396c322ab2e1eb966e6
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-17 09:37:56 -0600

    Starting faraday connection.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,11 @@
 class SearchController < ApplicationController
   def index
+    conn = Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC,LPG&limit=10") do
+      faraday.headers["X-Api-Key"] = "#{ENV["nrel_api_key"]}"
+      faraday.adapter Faraday.default_adapter
+    end
+    
+
+    @stations 
   end
 end
