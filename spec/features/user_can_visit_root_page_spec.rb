2b9e56393500871b700af505592fe9dca8d42201
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-10-17 09:21:26 -0600

    adds search controller and view.

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -8,4 +8,18 @@ feature "User can visit root page" do
       expect(page).to have_selector("input[value='Search by zip...']")
     end
   end
+  scenario "and search for 10 closest stations" do
+    # As a user
+    # When I visit "/"
+    visit "/"
+    # And I fill in the search form with 80203 (Note: Use the existing search form)
+    fill_in :q, with: 80203
+    # And I click "Locate"
+    click_on 'Locate'
+    # Then I should be on page "/search"
+    expect(current_path).to eq('/search')
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    # And the stations should be limited to Electric and Propane
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+  end
 end
